43d3ac43232b4f3a5d77b1130f4b75d6bd554ca4
Ankit <ankityc143>
2020-01-14 23:08:06 +0530

    Updates

---
+++ precache-manifest.f000cde3ba9338639ec34ea69c28e57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2a143cb2136cf5b759eee71bd4f908",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "a57ab34440f3b5d44bd4",
+    "url": "/tictactoe-react/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "d1f51e66b17b6b2638f6",
+    "url": "/tictactoe-react/static/js/2.5a6bea28.chunk.js"
+  },
+  {
+    "revision": "a57ab34440f3b5d44bd4",
+    "url": "/tictactoe-react/static/js/main.4e01a5d9.chunk.js"
+  },
+  {
+    "revision": "fc3dc3fc111c4d693df0",
+    "url": "/tictactoe-react/static/js/runtime~main.70b946e9.js"
+  }
+]);
